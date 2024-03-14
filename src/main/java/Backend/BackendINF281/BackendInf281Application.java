e336c6b26110704bc606b2cb6430e40a124b169c
EdiSteven <ediroyer2002@gmail.com>
2024-03-13 20:45:31 -0400

    commit inicial del proyecto

---
+++ src/main/java/Backend/BackendINF281/BackendInf281Application.java
@@ -0,0 +1,13 @@
+package Backend.BackendINF281;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInf281Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInf281Application.class, args);
+	}
+
+}
---
+++ src/test/java/Backend/BackendINF281/BackendInf281ApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.BackendINF281;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInf281ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
